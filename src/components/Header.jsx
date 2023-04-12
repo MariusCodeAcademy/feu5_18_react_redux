941c466bb33f0f1a4f2df2302f8b979a49f0e7d2
Marius CA <mariuskcodeacademy@gmail.com>
2023-04-12 12:43:19 +0300

    header logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Card from './ui/card/Card';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { authActions } from '../store';
 
 function Header() {
   const isAuth = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+  // logout in header
+  function logoutHandler() {
+    dispatch(authActions.logout());
+  }
   return (
     <header>
       <Card>
         {!isAuth && <p>No user is logged in</p>}
-        {isAuth && <button>Logout</button>}
+        {isAuth && <button onClick={logoutHandler}>Logout</button>}
       </Card>
     </header>
   );
